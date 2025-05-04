7a1fba81092f57cbfc53dc73a83b0a4e50b312db
K-M-PERERA <147619333+K-M-PERERA@users.noreply.github.com>
2025-05-05 01:08:35 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,17 +5,6 @@ import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyBUBaAL4v94FpfxRGAFK-u3cvMcoP6KMlU",
-
-  authDomain: "streetsense-f9eb0.firebaseapp.com",
-
-  projectId: "streetsense-f9eb0",
-
-  storageBucket: "streetsense-f9eb0.firebasestorage.app",
-
-  messagingSenderId: "966948358596",
-
-  appId: "1:966948358596:web:83aeb8f3475bbf0b823c91"
 
 };
 
